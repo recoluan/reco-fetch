b56cb08ec610ae26848c8373b164b8469745ba00
reco_luan <luanhewei@gmail.com>
2019-01-14 10:06:20 +0800

    去掉入口js的箭头函数，修ie报错问题

--- src/index.js
+++ src/index.js
@@ -8,10 +8,9 @@ import addTimeout from './models/timeOut'
  * @param url    (String) 接口URL
  * @param option (Object) 参数对象，包括method(请求方式，不填默认'get')，headers(设置请求头，选填)，data(请求参数，所有请求方式均适用)
  */
-const recoFetch = (url, option = {}) => {
+function recoFetch (url, option = {}) {
   // 设置请求超时的时间，默认10秒
   const timeout = option.timeout || null
-  console.log(222222, timeout)
   const { recoUrl, recoOptions } = handleParams(url, option)
 
   return addTimeout(fetch(recoUrl, recoOptions), timeout)
