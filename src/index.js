a4ba841534ea37df9f044f2ff1af11eb8f6f0d37
reco_luan <luanhewei@gmail.com>
2019-01-19 16:28:36 +0800

    v1.0.5

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import addTimeout from './models/timeOut'
  * @param url    (String) 接口URL
  * @param option (Object) 参数对象，包括method(请求方式，不填默认'get')，headers(设置请求头，选填)，data(请求参数，所有请求方式均适用)
  */
-function recoFetch (url, option = {}) {
+window.recoFetch = (url, option = {}) => {
   // 设置请求超时的时间，默认10秒
   const timeout = option.timeout || null
   const { recoUrl, recoOptions } = handleParams(url, option)
@@ -16,4 +16,4 @@ function recoFetch (url, option = {}) {
   return addTimeout(fetch(recoUrl, recoOptions), timeout)
 }
 
-export default recoFetch
+export default window.recoFetch
